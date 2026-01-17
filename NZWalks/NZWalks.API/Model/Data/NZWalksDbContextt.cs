69946da85bf836769fc82a5b4c1a94e51da48673
Ravindra <shruthi92mr@gmail.com>
2026-01-17 16:26:38 +0530

    commit stage

---
+++ NZWalks/NZWalks.API/Model/Data/NZWalksDbContextt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.DomainModel;
+
+
+namespace NZWalks.API.Model.Data
+{
+    public class NZWalksDbContextt:DbContext
+    {
+        public NZWalksDbContextt(DbContextOptions<NZWalksDbContextt> options):base(options)
+        {
+
+        }
+        
+       public DbSet<Region> Regions { get; set; }   
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Model/DomainModel/NZWalks.cs
@@ -0,0 +1,29 @@
+﻿namespace NZWalks.API.Model.DomainModel
+{
+   
+
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string RegionImageUrl { get; set; }
+    }   
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }   
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        // Navigation Properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }   
+}
